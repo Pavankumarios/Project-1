7e5698f5ecd87dffef17c2472b7c343ae2bc2fb6
Pavankumarios <kvpavan4481@gmail.com>
2022-12-08 12:28:21 +0530

    second commit

--- Project-1/ViewController.swift
+++ Project-1/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("addes")
+        print("addes")
+        print("addes")
+        print("addes")
+
         // Do any additional setup after loading the view.
     }
 
